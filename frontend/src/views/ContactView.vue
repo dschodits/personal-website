0e3465f2b0afade386e20b8e097626c10ae99900
dschodits <dschodits@student.tgm.ac.at>
2023-11-16 15:41:09 +0100

    contact page done

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -11,9 +11,19 @@ import ContactElement from '../components/ContactElement.vue';
       <Card>
         <template #title> Contact </template>
           <template #content>
-            <ContactElement msg="Daymon Schodits" icon="pi pi-linkedin" link="https://www.linkedin.com/in/daymon-schodits-45145a226/"/>
-            <ContactElement msg="Daymon Schodits" icon="pi pi-github" link="https://github.com/dschodits"/>
-            <ContactElement msg="Daymon Schodits" icon="pi pi-envelope"/>
+            <div class="flex justify-center">
+              <div>
+                <ContactElement msg="Daymon Schodits" icon="pi pi-linkedin" link="https://www.linkedin.com/in/daymon-schodits-45145a226/"/>
+                <ContactElement msg="dschodits" icon="pi pi-github" link="https://github.com/dschodits"/>
+                <ContactElement msg="dschodits@gmail.com" icon="pi pi-envelope"/>
+              </div>
+            </div>
+            <div class="flex flex-row justify-center">
+              <Image src="/wien.jpg" width="400" height="400" alt="Image"/>
+            </div>
+            <div class="flex justify-center">
+              Located in Vienna
+            </div>
           </template>
 
       </Card>
